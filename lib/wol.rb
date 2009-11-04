934983284655999dd711f058d576b1dbb6f8c900
Matias Korhonen <korhonen.matt@gmail.com>
2009-11-04 16:56:30 +0200

    Modified the WakeOnLan class to be more suitable for gemification.

--- lib/wol.rb
+++ lib/wol.rb
@@ -9,12 +9,27 @@
 #
 require "socket"
 
-# Ruby version of the WakeOnLan command.  Originally written by K.Kodama.
+# Ruby version of the WakeOnLan command.
 class WakeOnLan
-  attr :socket
+  attr_accessor :mac, :address, :port, :count, :interval, :verbose
+  attr_reader :socket
 
-  # Create a new instance. No arguments required.
-  def initialize
+  # Create a new instance
+  # == Options
+  # * <tt>:mac => "ff:ff:ff:ff:ff:ff"</tt> - Specify the destination MAC Address. Defaults to the broadcast MAC, "ff:ff:ff:ff:ff:ff"
+  # * <tt>:address => "255.255.255.255"</tt> - Specify the destination address.  Either a IP or hostname.  Defaults to "255.255.255.255"
+  # * <tt>:port => 9</tt> - The destination port. Defaults to the discard port, 9
+  # * <tt>:count => 1</tt> - How many times to send the MagicPacket.  Defaults to 1
+  # * <tt>:interval => 0.01</tt> - How many seconds to wait between sending packets. Defaults to 0.01
+  # * <tt>:verbose => false</tt> - What to return?  Returns a string summary if true, else returns <tt>:count</tt>.
+  def initialize(options = {})
+    @mac = options[:mac] ||= "ff:ff:ff:ff:ff:ff"
+    @address = options[:address] ||= "255.255.255.255"
+    @port = options[:port] ||= 9
+    @count = options[:count] || 1
+    @interval = options[:interval] || 0.01
+    @verbose = options[:verbose] || true
+
     @socket=UDPSocket.open()
     @socket.setsockopt(Socket::SOL_SOCKET,Socket::SO_BROADCAST,1)
   end
@@ -26,38 +41,23 @@ class WakeOnLan
   end
 
   # Wake a host.
-  #
-  # == Options
-  # * <tt>:mac => "ff:ff:ff:ff:ff:ff"</tt> - Specify the destination MAC Address. Defaults to the broadcast MAC, "ff:ff:ff:ff:ff:ff"
-  # * <tt>:address => "255.255.255.255"</tt> - Specify the destination address.  Either a IP or hostname.  Defaults to "255.255.255.255"
-  # * <tt>:port => 9</tt> - The destination port. Defaults to the discard port, 9
-  # * <tt>:count => 1</tt> - How many time to send the MagicPacket.  Defaults to 1
-  # * <tt>:interval => 0.01</tt> - How many seconds to wait between sending packets. Defaults to 0.01
-  # * <tt>:verbose => false</tt> - What to return?  Returns a string summary if true, else returns <tt>:count</tt>.
-  def wake(options = {})
-    mac = options[:mac] ||= "ff:ff:ff:ff:ff:ff"
-    address = options[:address] ||= "255.255.255.255"
-    port = options[:port] ||= 9
-    count = options[:count] || 1
-    interval = options[:interval] || 0.01
-    verbose = options[:verbose] || false
+  def wake
+    magicpacket = (0xff.chr)*6+(@mac.split(/:/).pack("H*H*H*H*H*H*"))*16
 
-    magicpacket = (0xff.chr)*6+(mac.split(/:/).pack("H*H*H*H*H*H*"))*16
+    @count.times {
+      @socket.send(magicpacket, 0, @address, @port)
 
-    count.times {
-      @socket.send(magicpacket, 0, address, port)
-
-      sleep interval if interval > 0 unless count == 1
+      sleep @interval if @interval > 0 unless @count == 1
     }
 
-    if verbose
-      if count == 1
-        return "Sending magic packet to #{address}:#{port} with #{mac}"
+    if @verbose
+      if @count == 1
+        return "Sending magic packet to #{@address}:#{@port} with #{@mac}"
       else
-        return "Sending magic packet to #{address}:#{port} with #{mac} #{count} times"
+        return "Sending magic packet to #{@address}:#{@port} with #{@mac} #{@count} times"
       end
     else
-      return count
+      return @count
     end
   end
 end
