cb34752a9d6f25974d658469d729e90f4dd94509
Matias Korhonen <korhonen.matt@gmail.com>
2009-11-09 17:40:38 +0200

    Restructured the file structure

---
+++ lib/wol/parsefile.rb
@@ -0,0 +1,54 @@
+# Parse a text file containing hardware addresses, host names/addresses, and port numbers
+module Wol
+  module ParseFile
+
+    # Parse a given string and return a hash containing the results
+    def self.parse(text)
+      hosts = []
+      text.each_line do |line|
+        unless line.match(/^#/) || line.strip.empty?
+          mac, address, port = line.strip.split
+
+          port ||= 9
+          host ||= "255.255.255.255"
+
+          if check_mac(mac) && check_host(address)
+            hosts << { :mac => mac, :address => address, :port => sanitize_port(port)}
+          end
+        end
+      end
+
+      return hosts
+    end
+
+    # Read a file and then parse its contents
+    def self.read_and_parse_file(file)
+      parse(read_file(file))
+    end
+
+    private
+
+    # Read a file and return the contents
+    def self.read_file(file)
+      if File.exist?(file) && File.readable?(file)
+        File.open(file, "r").read
+      end
+    end
+
+    # Check if a given hardware address is correctly formed.
+    def self.check_mac(mac)
+      /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/.match(mac)
+    end
+
+    # Check that a host/ip address doesn't contain any illegal characters.
+    def self.check_host(host)
+      /(^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$)|(^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$)/.match(host)
+    end
+
+    # Make sure that a given port number is an integer in the correct range (1..61000).
+    def self.sanitize_port(port)
+      p = port.to_i
+      return (1..61000).include?(p) ? p : 9
+    end
+  end
+end
---
+++ lib/wol/runner.rb
@@ -0,0 +1,149 @@
+require 'optparse'
+
+module Wol
+  module Runner
+    @sample_file = %{# File structure
+# --------------
+# - blank lines are ignored
+# - comment lines are ignored (lines starting with a hash mark '#')
+# - other lines are considered valid records and can have 3 columns:
+#
+#       Hardware address, address, destination port
+#
+#   the last two are optional, in which case the following defaults
+#   are used:
+#
+#       IP address: 255.255.255.255 (the limited broadcast address)
+#       port:       9 (the discard port)
+#
+
+01:02:03:04:05:06   192.168.1.255       9
+07:09:09:0A:0B:0C   example.com
+0D:0E:0F:00:10:11}
+
+    def self.parse(args)
+      args = ["-h"] if args.empty?
+
+      options = {}
+      options[:quiet] = false
+      options[:address] = "255.255.255.255"
+      options[:port] = 9
+      options[:interval] = 0.01
+      options[:count] = 3
+      options[:macs] = Array.new
+      options[:file] = nil
+
+      opts = OptionParser.new do |opts|
+        # Set a banner, displayed at the top
+        # of the help screen.
+        opts.banner = "#{version}\nUsage: wol -a ADDRESS ff:ff:ff:ff:ff:ff"
+
+        opts.separator ""
+        opts.separator "Specific options:"
+
+        opts.on( '-q', '--quiet', 'No console output' ) do
+          options[:quiet] = true
+        end
+
+        opts.on( '-a', '--address 255.255.255.255', 'Set the destination address' ) do |address|
+          options[:address] = address
+        end
+
+        opts.on( '-p', '--port 9', Integer, 'Set the destination port' ) do |port|
+          options[:port] = port
+        end
+
+        opts.on( '-i', '--interval 0.01', Float, 'Interval between sending packets in seconds') do |interval|
+          options[:interval] = interval
+        end
+
+        opts.on( '-c', '--count 3', Integer, 'Number of packets to send. Default 3') do |count|
+          options[:count] = count
+        end
+
+        opts.on( '-f', '--file FILE', 'TODO: Uses a file as a source of addresses') do |file|
+          options[:file] = file
+        end
+
+        opts.separator ""
+        opts.separator "Common options:"
+
+        opts.on_tail( '-h', '--help', 'Display this message' ) do
+          puts opts
+          exit
+        end
+
+        opts.on_tail( '-s', '--sample-file', 'Display a sample file') do
+          puts @sample_file
+          exit
+        end
+
+        opts.on_tail( '-v', '--version', 'Show version') do
+          puts version
+          exit
+        end
+      end
+
+      begin
+        opts.parse!(args)
+
+        if options[:file].nil?
+          args.each do |arg|
+            options[:macs] << arg if /^(\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2}:\S{1,2})?$/.match(arg)
+          end
+
+          options[:macs].uniq!
+        end
+
+        return options
+      rescue OptionParser::InvalidOption => e
+        STDERR.puts e.message, "\n", opts
+        exit(-1)
+      end
+    end
+
+    def self.version
+      "Ruby Wake-On-LAN version 0.2.2"
+    end
+
+    def self.wake(options = {})
+      begin
+        if options[:file].nil?
+          if !options[:macs].empty?
+            puts  WakeOnLan.new(options).wake unless options[:quiet]
+          else
+            puts "You have to specify a file or MAC address"
+          end
+        else
+          puts wake_from_file(options) unless options[:quiet]
+        end
+      rescue Exception => e
+        puts "An error occured. Please check your inputs."
+        puts "If you used a file, please check that it is properly formatted."
+        STDERR.puts e.message
+        exit(-1)
+      end
+    end
+
+    def self.wake_from_file(options = {})
+      message = ""
+      hosts = ParseFile.read_and_parse_file(options[:file])
+
+      for host in hosts
+        options[:address], options[:macs], options[:port] = host[:address], host[:mac], host[:port]
+
+        unless options[:quiet]
+          message << WakeOnLan.new(options).wake
+        end
+      end
+
+      return options[:quiet] ? nil : message
+    end
+
+    def self.run!
+      options = parse(ARGV)
+
+      wake(options)
+    end
+  end
+end
